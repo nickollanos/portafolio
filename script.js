6d7bbd8050d02c671fbf4f0177625226e5a3ecad
nickollanos <nico26928@hotmail.com>
2025-01-24 15:36:22 -0500

    faltan detallitos

--- script.js
+++ script.js
@@ -20,19 +20,24 @@ function seleccionar(){
 //funcion de animaciones habilidades
 function efectoHabilidades(){
  var skills =  document.getElementById("skills");
+ console.log(skills.getBoundingClientRect().top);
+ 
  var distanciaSkills =  window.innerHeight - skills.getBoundingClientRect().top;
+ console.log(distanciaSkills);
  if(distanciaSkills >= 300){
     let habilidades = document.getElementsByClassName("progreso");
-    habilidades[0].classList.add("javascript");
+        habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("htmlcss");
-        habilidades[2].classList.add("photoshop");
-        habilidades[3].classList.add("wordpress");
-        habilidades[4].classList.add("drupal");
-        habilidades[5].classList.add("comunicacion");
-        habilidades[6].classList.add("trabajo");
-        habilidades[7].classList.add("creatividad");
-        habilidades[8].classList.add("dedicacion");
-        habilidades[9].classList.add("proyect");
+        habilidades[2].classList.add("laravel");
+        habilidades[3].classList.add("php");
+        habilidades[4].classList.add("mysql");
+        habilidades[5].classList.add("angular");
+        habilidades[6].classList.add("comunicacion");
+        habilidades[7].classList.add("trabajoenequipo");
+        habilidades[8].classList.add("creatividad");
+        habilidades[9].classList.add("dedicacion");
+        habilidades[10].classList.add("responsabilidad");
+        console.log(habilidades);
  }
 }
 
